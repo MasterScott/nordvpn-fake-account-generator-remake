2dc18ef2de07f88f9f37c497fe8aeaece3823fc5
miso-xyz <61099345+miso-xyz@users.noreply.github.com>
2020-05-01 21:55:26 +0200

    Add files via upload

---
+++ NordVPN Generator/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 11.0!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Location = New System.Drawing.Point(84, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 33)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "By Alphacrack"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.NordVPN_Generator.My.Resources.Resources.MainLogo
+        Me.PictureBox1.Location = New System.Drawing.Point(58, 77)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(190, 203)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(44, 353)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(204, 27)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.TextBox1.Location = New System.Drawing.Point(35, 304)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(228, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 11.0!, System.Drawing.FontStyle.Bold)
+        Me.Button2.Location = New System.Drawing.Point(71, 425)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(145, 35)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "GENERATE KEY"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 43
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(121, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(302, 501)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "NordVPN  PREMIUM KEY GENERATOR V1.4"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ NordVPN Generator/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ProgressBar1.Value += 1
+        If ProgressBar1.Value = 100 Then
+            ProgressBar1.Value = 0
+            Dim rng As New Random
+            TextBox1.Text = accounts(rng.Next(0, accounts.Count))
+            Timer1.Stop()
+            MessageBox.Show("Account PREMIUM ENJOY!!", "NordVPN Generator")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Timer1.Start()
+    End Sub
+
+    Dim accounts As New List(Of String)({ _
+                                                "80.hengst@gmail.com:Fatdump1",
+                                                "brantharry@yahoo.com.au:sunnyboy7",
+                                                "IDG4EVER@GMAIL.COM:93CORRADO",
+                                                "Xboxrules2004@yahoo.com : Spartan2s",
+                                                "djwolfi@gmx.de : Mw270980",
+                                                "douglasweilee@gmail.com : shinn105",
+                                                "dmalone35@yahoo.com : cincity27",
+                                                "cupra66@hotmail.fr : evann1910",
+                                                "claymayo@hotmail.com : Luckyjerry1",
+                                                "dewijones.75@gmail.com:Johanne09",
+                                                "dynzo1@gmail.com : P0w3rth1rs7",
+                                                "colinzelin@gmail.com : mets5000",
+                                                "dannymreed@live.com : Oou812B612",
+                                                "damsgaard.jonas@gmail.com:41erikwowerik",
+                                                "elia.knudsen@gmail.com : luisa2009",
+                                                "elliott.rieves@gmail.com : Lilrayray03",
+                                                "chickenisafingerfood@gmail.com : Jimbobjoe1",
+                                                "djiby.sakho@free.fr : djibysakho",
+                                                "daniel.douglas101@gmail.com : Minecraf1",
+                                                "commo@hotmail.co.uk : Xxcpegh123",
+                                                "dbeere3@gmail.com : Redstripe1",
+                                                "demonic_sage@hotmail.com:3bLJJiGW",
+                                                "ellissafincken@hotmail.com : Liliana1",
+                                                "davidmullis06@aol.com : bluenose1875",
+                                                "davidburtonor@gmail.com : Echo2134",
+                                                "elijah.harrison.evans@gmail.com : Slacker52",
+                                                "emobruenski@gmail.com : Cupoftea1",
+                                                "dirk@daude.net : henner70",
+                                                "charles.chapel@gmail.com : Charles95",
+                                                "erodgers37@mybc3.bc3.edu : MUMmum123",
+                                                "erik.a.gademsky@gmail.com : Godspeed1",
+                                                "enno@olbrich.net:94f6291z",
+                                                "eliejuniorbadr@gmail.com : Elieelie1",
+                                                "frankcanfield7@gmail.com : Frankdad69",
+                                                "fontmail1@aol.com : jamaica1",
+                                                "erisaafshar@gmail.com : Artais21",
+                                                "geocon@iinet.net.au:494maria"
+                                                })
+End Class
